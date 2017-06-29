d0c76cf54d6c54c960f5a556cb8a269c84187a71
Romserg <serg.romankov@gmail.com>
2017-06-29 15:06:15 +0300

    well done

--- src/index.js
+++ src/index.js
@@ -1,1 +1,52 @@
-﻿
+﻿function min(Array){
+	if(Array===undefined){
+	return;
+	} else {
+		var minEl;
+		for(var i=0;i<Array.length;i++){
+ 			if (typeof Array[i] == 'number'){
+   				if (minEl===undefined ){
+     			minEl = Array[i];
+    			 continue;
+   				}
+   			if(minEl>=Array[i]){
+    		console.log(minEl)
+  			minEl=Array[i];}
+  			}
+			
+		}
+		return minEl;
+	}
+}
+
+function max(Array){
+	if(Array===undefined){
+	return;
+	} else {
+		var maxEl=Array[0];
+		for(var i=0;i<Array.length;i++){
+			if(Array[i]>=maxEl)
+			maxEl=Array[i]
+		}
+		return maxEl;
+	}
+}
+
+function sum(){
+	if(arguments===undefined){
+	return;
+	} else {
+		var summ=0;
+		for(var i=0;i<arguments.length;i++){
+			if(typeof arguments[i] !== 'number')
+			continue;
+			summ+=arguments[i];
+		}
+		return summ;
+	}
+}
+
+
+console.log(min([]));
+console.log(max());
+console.log(sum());
